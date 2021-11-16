c457c53dd9c233fe03ee63650aaa9a67b436543f
yangfan <yangfan9702@outlook.com>
2021-11-16 15:10:25 +0800

    format

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import re
 import os
 from setuptools import setup, find_packages  # type: ignore
@@ -13,49 +12,44 @@ def read(fname):
 
 
 def desc():
-    return read('README.md')
+    return read("README.md")
 
 
 # grep cibo/__init__.py since python 3.x cannot
 # import it before using 2to3
-file_text = read(fpath('cibo/__init__.py'))
+file_text = read(fpath("cibo/__init__.py"))
 
 
 def grep(attrname):
     pattern = r'{0}\W*=\W*"([^\']+)"'.format(attrname)
-    strval, = re.findall(pattern, file_text)
+    (strval,) = re.findall(pattern, file_text)
     return strval
 
 
 setup(
-    name='cibo',
-    version=grep('__version__'),
-    license='MIT',
-    author=grep('__author__'),
-    author_email=grep('__email__'),
-    description='a CBV(class base views) web framework based on flask',
+    name="cibo",
+    version=grep("__version__"),
+    license="MIT",
+    author=grep("__author__"),
+    author_email=grep("__email__"),
+    description="a CBV(class base views) web framework based on flask",
     long_description=desc(),
     long_description_content_type="text/markdown",
     packages=find_packages(
-        exclude=[
-            'tests', 'tests.*',
-            'examples', 'examples.*',
-            'demo', 'demo.*',
-            'etc', 'etc.*'
-        ]
+        exclude=["tests", "tests.*", "examples", "examples.*", "demo", "demo.*", "etc", "etc.*"]
     ),
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
+    platforms="any",
     install_requires=[
         "Flask==1.1.1",
         "pydantic==1.6.1",
         "flasgger==0.9.3",
     ],
     classifiers=[
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7.5',
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7.5",
     ],
     # entry_points={
     #     'flask.commands': [
