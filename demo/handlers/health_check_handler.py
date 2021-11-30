7404d82e0cc92ed44797c46156b072e89525c999
yangfan <yangfan9702@outlook.com>
2021-11-30 21:05:05 +0800

    add resp to demo

--- demo/handlers/health_check_handler.py
+++ demo/handlers/health_check_handler.py
@@ -1,9 +1,15 @@
+from cibo.args import BaseApiSuccessResp
+from typing_extensions import Literal
 from cibo import Handler, SimpleContext
 from demo.handlers import api
 
 
 @api.get("/ping")
 class PingHandler(Handler):
+    
+    class Resp(BaseApiSuccessResp):
+        msg: Literal["pong"]
+
     def handle(self, context: SimpleContext):
         """return `pong` if service work normally"""
         return context.success(msg="pong")
